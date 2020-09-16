9f69e86a30ee01e705504e50c9d83fc227e87ae8
lianchua <50864543+lianchua@users.noreply.github.com>
2020-09-16 17:42:05 +0800

    Update content.js
    修复了因为长按持续触发刷新的bug，现在刷新之前，会把flag改为false，从而拦截掉后面的所有刷新，直到刷新后，js又重新触发和执行；

--- js/content.js
+++ js/content.js
@@ -1,50 +1,40 @@
 /**
- * Created by Administrator on 2020年9月14日.
+ * Created by Administrator on 2020年9月16日.
  */
 window.onload = function () {
-    // 获取当前页面的url
     var url = location.href;
-    // 定下一个flag 默认值为false
-    let flag = false
-    // 延时执行程序，1秒钟后把flag变为true
+    let flag = false;
     setTimeout(() => {
-        flag = true
+        flag = true;
     }, 1000);
-    //全局按键监听
+    //实际使用 
     document.onkeydown = function (e) {  //对整个页面文档监听 
         var keyNum = window.event ? e.keyCode : e.which;  //获取被按下的键值 
         //判断如果用户按下了回车键（keycody=13）
         // alert(keyNum);
         // console.log('key' + keyNum)
-        // 如果按键键值等于13并且flag为true的情况执行if内函数
-        if (keyNum == 13 && flag) {
-            console.log('键值正确，且flag为true')
-            // 判断当前页面的url是否是我们想要实现按住刷新的url
-            if (url.indexOf('http://地址已经进行加密处理/test.html') != -1) {
-                // 捕获异常
+
+        if (keyNum == 107 && flag) {
+            console.log('我要刷新');
+            if (url.indexOf('http://192.168.0.1/test.html') != -1) {
                 try {
-                    //获取页面上某个指定的class名字的dom对象
-                    var dom = document.getElementsByClassName('className')
+                    var dom = document.getElementsByClassName('className');
                     // console.log(dom)
                     // console.log(dom.length)
                 } catch (error) {
-                    // 如果报错了，就发送0到后台
-                    chrome.runtime.sendMessage(0, function (response) {
-                        console.log('content get response:', response);
-                    });
-                    window.location.href = window.location.href // 刷新页面
+                    chrome.runtime.sendMessage(0);
+                    window.location.href = window.location.href;
                 }
-                // 通过dom对象的length判断其是否存在，存在length>0
                 if (dom.length > 0) {
-                    chrome.runtime.sendMessage(1, function (response) {
-                        console.log('content get response:', response);
-                    });
-                    return; //既然找到了，就结束程序执行吧！
-                } else if (dom.length == 0) { //不存在就==0
-                    chrome.runtime.sendMessage(0, function (response) {
-                        console.log('content get response:', response);
-                    });
-                    window.location.href = window.location.href // 刷新页面
+                    chrome.runtime.sendMessage(1);
+                    return;
+                } else if (dom.length == 0) {
+                    chrome.runtime.sendMessage(0);
+                    flag = false;
+                    console.log(flag);
+                    window.location.href = window.location.href;
+                    console.log(flag);
+                    return;
                 }
             }
         }
